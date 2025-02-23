2aa419ce28080f71033a28f8d81d4fef39b7b78d
Momad <s_lpln@yahoo.com>
2025-02-23 13:02:22 +0330

    #

---
+++ SimpleQuiz/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SimpleQuiz
+{
+    public class ApplicationUser : IdentityUser<int>, IHasExtraInfo
+    {
+        public string? FullName { get; set; }
+        public string? Bio { get; set; }
+        public string? WebSite { get; set; }
+        public DateTime BirthDay { get; set; }
+        public bool Gender { get; set; }
+    }
+}
